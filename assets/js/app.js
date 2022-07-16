00bd84d51abbb9f8e4ec08314759639bc4687bc1
Marksolidor <marco350sl@gmail.com>
2022-07-15 22:06:12 -0400

    feat baseline

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+//Construye una página web con las siguientes características (Total: 5 puntos)
+//● Crea 4 divs que tengan alto y ancho de 200px y de distintos colores de fondo
+//usando el atributo style. Cada uno de los divs debe tener un identificador
+//único.
+//● Crea un script que guarde dentro de una variable global un color
+//dependiendo de la letra del teclado presionada. (2 Puntos).
+//○ Al presionar la letra a un color a tu elección.
+//○ Al presionar la letra s un segundo color a tu elección.
+//○ Al presionar la letra d un tercer color al presionar la letra d.
+//○ Para guardar el color revisa el tip al final del enunciado.
+//● Dentro del script agrega el evento que al hacerle click a uno de los divs, este
+//cambie de color al color seleccionado. Utiliza addEventListener para agregar
+//el evento. (3 Puntos)
