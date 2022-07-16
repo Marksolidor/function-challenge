4d4fd0faba5c8a563fc0898761c1cbfca5e2de42
Marksolidor <marco350sl@gmail.com>
2022-07-15 21:22:32 -0400

    feat arrow function

---
+++ assets/js/2_arrow.js
@@ -0,0 +1,8 @@
+//2. Transforma las siguiente función a una arrow function de una línea. Este ejercicio
+//debe quedar en un archivo llamado `2 arrow.js`
+//suma = function(a, b){
+//return a + b}
+
+let suma = (a, b) => (return a + b);
+    
+    
