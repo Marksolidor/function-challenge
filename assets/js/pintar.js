cece228f8bfe6dffad1cdc991de540b002921486
Marksolidor <marco350sl@gmail.com>
2022-07-15 21:29:55 -0400

    feat baseline

---
+++ assets/js/pintar.js
@@ -0,0 +1,14 @@
+//3. Se tiene como base el siguiente ejercicio que cambia el color de un elemento de
+//HTML al hacerle click
+//3.1 Modifica la función para que reciba el elemento clickeado de forma de no
+//tener que seleccionarlo nuevamente dentro de la función (1 Punto). Para
+//obtener el puntaje debes entregar el 3 pintar.html válido funcionando en
+//conjunto con el código modificado.
+
+function pintar(){
+    ele = document.getElementById("ele1")
+    ele.style.backgroundColor = 'yellow'
+    }
+    ele = document.getElementById("ele1")
+    ele.addEventListener("click", pintar);
+    
